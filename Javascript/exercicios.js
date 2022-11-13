f85d47de2a73cc56515b7c3f1d06dadcce7d4564
Israel Alves <ysrael.2009@hotmail.com>
2022-11-13 19:32:43 -0300

    algotimo de compra

--- Javascript/exercicios.js
+++ Javascript/exercicios.js
@@ -58,3 +58,26 @@ if (imc < 18.5) {
 
 
 console.log(imc.toFixed(2));
+
+/*
+3) Elabore um algoritmo que calcule o que deve ser pago por um produto, considerando o preço normal de etiqueta e a escolha da condição de pagamento.
+
+Código Condição de pagamento:
+1 - À vista débito, recebe 10% de desconto;
+2 - À vista no dinheiro ou Pix, recebe 15% de desconto;
+3 - Em duas vezes, preço normal de etiqueta sem juros;
+4 - Acima de duas vezes, preço normal de etiqueta mais juros de 10%;
+*/
+
+const precoEtiqueta = 100;
+const formaDePagar = 4;
+
+if (formaDePagar === 1) {
+  console.log(precoEtiqueta - (precoEtiqueta * 0.1));
+} else if (formaDePagar === 2) {
+  console.log(precoEtiqueta - (precoEtiqueta * 0.15));
+} else if (formaDePagar === 3) {
+  console.log(precoEtiqueta);
+} else {
+  console.log(precoEtiqueta + (precoEtiqueta * 0.1))
+}
