e3ff4217ec4701db0b8343f17a438b45fc2db4ac
Israel Alves <ysrael.2009@hotmail.com>
2022-12-05 00:13:13 -0300

    pokemon list

--- POKEDEX/assets/js/main.js
+++ POKEDEX/assets/js/main.js
@@ -2,7 +2,33 @@ const offset = 0;
 const limit = 10;
 const url = `https://pokeapi.co/api/v2/pokemon/?offset=${offset}&limit=${limit}`
 
+function convertPokemonToLi(pokemon) {
+  return `
+    <li class="pokemon">
+      <span class="number">#001</span>
+      <span class="name">${pokemon.name}</span>
+
+      <div class="details">
+        <ol class="types">
+          <li class="type">grass</li>
+          <li class="type">poison</li>
+        </ol>
+
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="${pokemon.name}">
+      </div>
+    </li>
+  `
+}
+
+const pokeList = document.querySelector('#pokeList')
+
 fetch(url)
   .then(res => res.json())
-  .then(jsonBody => console.log(jsonBody))
+  .then(jsonBody => jsonBody.results)
+  .then(pokemons => {
+    for (let i = 0; i < pokemons.length; i++) {
+      const pokemon = pokemons[i];
+      pokeList.innerHTML += convertPokemonToLi(pokemon)
+    }
+  })
   .catch(error => console.log(error))
